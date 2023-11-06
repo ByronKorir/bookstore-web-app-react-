82d3287db262976d02141d75739ce1f7ce56e878
byron korir <byron.korir@student.moringaschool.com>
2023-11-06 13:02:40 +0300

    update bookcards component

--- src/components/BookCards.js
+++ src/components/BookCards.js
@@ -1,14 +1,35 @@
 import React, {useState, useEffect} from 'react'
+import '../styles/BookCard.css'
 
 export default function BookCards() {
    const [books, setBooks] = useState([]);
 
    useEffect(() => {
-     fetch(``)
+     fetch(`http://localhost:8001/books`)
+     .then((res)=> res.json())
+     .then((data)=>{
+      setBooks(data)
+     })
    }, [])
+   console.log(books)
    
 
   return (
-    <div>BookCards</div>
+    <div className='container row mx-auto'>
+      {
+         books.map((book)=>(
+         <div key={book.id} className='col-md-2 card mb-3 ms-5 text-center'>
+            <div className='pt-1'>
+               <img id='book-card-cover' className='img-fluid' src={book.cover} alt={book.title}/>
+            </div>
+            <div id='description' className='d-flex'>
+               <p>Genre:<span></span>{book.genre}</p>
+               <p>Price:<span>{book.price}</span></p>
+            </div>
+         </div>
+         ))
+      }
+      
+    </div>
   )
 }
