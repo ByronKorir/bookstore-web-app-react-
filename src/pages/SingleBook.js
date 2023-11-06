ae46280aae3a7fe86e594ec4f86d664857c4d774
byron korir <byron.korir@student.moringaschool.com>
2023-11-06 15:55:25 +0300

    style singlebook

--- src/pages/SingleBook.js
+++ src/pages/SingleBook.js
@@ -15,22 +15,24 @@ export default function SingleBook() {
   
    
   return (
-    <div id='single-book' className='mx-auto'>
-      <div id='image'>
-         <img className='img-fluid' src={book.cover} alt={book.title}/>
-      </div>
-      <div className='text-center'>
-         <p>{book.title}</p>
-         <p><strong>By:</strong><em>{book.author}</em></p>
-         <p>{book.description}</p>
-         <div>
-            <p>Genre:{book.genre}</p>
-            <p>Price: ksh/={book.price}</p>
-            <p>Available:{book.stock}</p>
+   <div id='main'>
+      <div id='single-book' className='mx-auto'>
+         <div id='image'>
+            <img className='img-fluid' src={book.cover} alt={book.title}/>
          </div>
+         <div id='main-des' className='text-start'>
+            <h2 className='text-success'><strong>{book.title}</strong></h2>
+            <p><strong>By:</strong><em>{book.author}</em></p>
+            <p>{book.description}</p>
+            <div id='descriptions'>
+               <p>Genre:<span className='text-success'>{book.genre}</span></p>
+               <p>Price: ksh/=<span className='text-success'>{book.price}</span></p>
+               <p>Available:<span className='text-success'>{book.stock}</span></p>
+            </div>
 
+         </div>
+         
       </div>
-      
     </div>
   )
 }
