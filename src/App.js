a2c2c423c085b859e24477a86812c9a75736f2aa
byron korir <byron.korir@student.moringaschool.com>
2023-11-06 11:13:34 +0300

    create add book page and navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './layout/Layout';
 import Home from './pages/Home'
+import AddBook from './pages/AddBook'
+import About from './pages/About'
+import Contacts from './pages/Contacts'
+
 
 function App() {
   return (
@@ -9,6 +13,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout/>}>
           <Route index element={<Home/>}/>
+          <Route path='/addbook' element={<AddBook/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contacts/>}/>
           
 
         </Route>
